c12a45cfb0cd4400696c109baa80773c9fb77615
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2023-02-25 01:48:14 -0300

    Create genbin.sh

---
+++ genbin.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+#25/02/2023
+#DEV: @RealStrategy
+
+#Contenedero de colores
+RED='\033[01;31m'
+YELLOW='\033[01;33m'
+CYAN='\033[01;36m'
+
+#Limpiar
+clear
+
+#Generador Inicial
+echo -e $RED "GENERADOR DE BIN PYTHON"
+echo ""
+echo -ne "\e[1;32mINGRESA BIN: ";
+read BIN;
+echo -ne "\e[1;34mCANTIDAD: ";
+read CANTIDAD;
+python2 .gencc.py -b $BIN -u $CANTIDAD -d -c
+echo -e $YELLOW "Presiona ENTER para volver a generar bin"
+echo -e $CYAN "Presiona CTRL C para salir!!"
+echo ""
+read foo
+bash genbin.sh
